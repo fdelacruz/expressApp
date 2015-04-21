f3a7dde0d294f39c7d5c1cd162d4292d72f0a1ff
Francisco De La Cruz <helloquico@gmail.com>
2015-04-21 00:00:31 -0400

    The Verb Methods

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 var express = require('express'),
+	bodyParser = require('body-parser'),
 	app 	= express();
 
+// GET     	- Read
+// POST 	- Create
+// PUT      - Update
+// DELETE   - Delete
+
+app.use(bodyParser.urlencoded({ extended: true })); // You have to explicitly set extended 'because option default will flip in next major'
+
+var names = [];
+
 app.get('/', function(req, res) {
-	res.render('index.jade', {
-		title: "Hello Express & Jade"
-	});
+	res.render('index.jade', { names: names });
+});
+
+app.post('/', function(req, res) {
+	names.push(req.body.name);
+	res.redirect('/');
 });
 
 app.listen(3000, function() {
